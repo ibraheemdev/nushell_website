27a6e9c4ae3946ef913c904c6d3c1ab8bc1d0b5e
Ibraheem <ibrah1440@gmail.com>
2020-11-14 14:42:11 -0500

    add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,6 +34,7 @@ module.exports = {
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    ["link", { rel: "icon", href: "/icon.png" }],
   ],
   themeConfig: {
     repo: "nushell/nushell",
